2405cb63080aa4d5280851ecf2b8cfea00168925
luncheon <takeuck+github@gmail.com>
2018-02-07 14:07:38 +0900

    refactor

--- input-clear-icon.js
+++ input-clear-icon.js
@@ -19,12 +19,11 @@
     password: IS_IE_OR_EDGE && 15,
   }
 
-  var previousResolvedStyles  = {}
   var clearIcon               = body.appendChild(doc.createElement('input-clear-icon'))
-  var clearIconStyle          = clearIcon.style
   var clearTargetElement
+  clearIcon.style.display     = 'none'
   clearIcon.onmousedown       = function (event) { event.preventDefault() } // prevent getting focus
-  clearIcon.onclick           = function() {
+  clearIcon.onclick           = function () {
     if (clearTargetElement) {
       clearTargetElement.value = ''
       var inputEvent = doc.createEvent('CustomEvent')
@@ -32,54 +31,49 @@
       clearTargetElement.dispatchEvent(inputEvent)
     }
   }
-  clearIconStyle.display      = 'none'
 
   function listener(event) {
     var targetElement           = event.target
-    var targetElementType       = targetElement.type
-    if (targetElement.tagName !== 'INPUT' || !TARGET_INPUT_TYPES[targetElementType]) {
+    if (targetElement.tagName !== 'INPUT' || !TARGET_INPUT_TYPES[targetElement.type]) {
       return
     }
     if ((targetElement.value || (targetElement.validity || {}).badInput) && doc.activeElement === targetElement && !targetElement.readOnly && !targetElement.disabled) {
       if (clearTargetElement !== targetElement) {
         clearTargetElement      = targetElement
+        clearIcon.removeAttribute('style')
         var targetClientRect    = targetElement.getBoundingClientRect()
         var targetStyle         = getComputedStyle(targetElement)
         var targetDataset       = targetElement.dataset
-        var resolvedStyles      = parseStyles(targetDataset.inputClearIconStyle)
-        Object.keys(previousResolvedStyles).forEach(function (styleProperty) { clearIconStyle[styleProperty] = '' })
+        var clearIconStyle      = clearIcon.style
         clearIcon.className     = targetDataset.inputClearIconClass || ''
         clearIconStyle.fontSize = targetStyle.fontSize
         clearIconStyle.zIndex   = parseInt(targetStyle.zIndex, 10) + 1
-        Object.keys(resolvedStyles).forEach(function (styleProperty) { clearIconStyle[styleProperty] = resolvedStyles[styleProperty] })
+        applyStyles(clearIconStyle, targetDataset.inputClearIconStyle)
         var clearIconSize       = parseInt(getComputedStyle(clearIcon).fontSize, 10)
-        clearIconStyle.left     = root.scrollLeft + body.scrollLeft + targetClientRect.right - clearIconSize
-                                - parseInt(targetStyle.borderRightWidth, 10) - parseInt(targetStyle.paddingRight, 10)
-                                - (ADDITIONAL_RIGHT_MARGINS[targetElementType] || 0)
-                                - 2
-                                + 'px'
         clearIconStyle.top      = root.scrollTop  + body.scrollTop  + targetClientRect.top + (targetClientRect.height - clearIconSize) / 2 + 'px'
-        clearIconStyle.display  = ''
-        previousResolvedStyles  = resolvedStyles
+        clearIconStyle.left     = root.scrollLeft + body.scrollLeft + targetClientRect.right - clearIconSize
+                                  - parseInt(targetStyle.borderRightWidth, 10) - parseInt(targetStyle.paddingRight, 10)
+                                  - (ADDITIONAL_RIGHT_MARGINS[targetElement.type] || 0)
+                                  - 2
+                                  + 'px'
       }
     } else {
+      clearIcon.style.display   = 'none'
       clearTargetElement        = undefined
-      clearIconStyle.display    = 'none'
     }
   }
 
-  function parseStyles(stylesString) {
-    return (stylesString ? stylesString.split(';') : []).reduce(function (styles, styleString) {
-      var separatorIndex = styleString.indexOf(':')
+  function applyStyles(style, stylesString) {
+    stylesString && stylesString.split(';').forEach(function (styleEntryString) {
+      var separatorIndex = styleEntryString.indexOf(':')
       if (separatorIndex !== -1) {
-        styles[camelCaseFromKebabCase(styleString.slice(0, separatorIndex).trim())] = styleString.slice(separatorIndex + 1).trim()
+        style[camelCaseFromKebabCase(styleEntryString.slice(0, separatorIndex).trim())] = styleEntryString.slice(separatorIndex + 1).trim()
       }
-      return styles
-    }, {})
+    })
   }
 
   function camelCaseFromKebabCase(kebabCaseString) {
-    return kebabCaseString.replace(/-([a-z])/g, function ($0, $1) { return $1.toUpperCase() })
+    return kebabCaseString.replace(/-[a-z]/g, function ($0) { return $0[1].toUpperCase() })
   }
 
   addEventListener('focus', listener, true)
